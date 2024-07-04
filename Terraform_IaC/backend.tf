ddcad5c00c23add7563d0c676c9ced5a67d137b2
box genius <115899706+box-genius@users.noreply.github.com>
2024-07-04 14:09:19 -0300

    pipeline teste

--- Terraform_IaC/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "projetos-terraform-status"
-    key            = "Minsait/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "nome-da-sua-tabela-dynamodb"
-    encrypt        = true
-  }
-}
--- Terraform_IaC/providers.tf
+++ Terraform_IaC/providers.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "projetos-terraform-status"
+    key            = "Minsait/terraform.tfstate"
+    region         = "us-east-1"
+    # dynamodb_table = "nome-da-sua-tabela-dynamodb"
+    # encrypt        = true
+  }
+}
